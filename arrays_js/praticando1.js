2a3b70b19e321a708fb1dd82548430bf6a7649d0
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-09-08 21:18:37 -0300

    Finalizando os primeiros exercicios de array, e trabalhando com novos metodos.

--- arrays_js/praticando1.js
+++ arrays_js/praticando1.js
@@ -1,5 +1,15 @@
 // Um array esparso é um tipo de array no qual a maioria dos elementos tem valores indefinidos. Crie um array esparso e atribua valores a alguns elementos do array (por exemplo, defina valores nos índices 1, 3 e 7). Imprima no console o array e também a informação de comprimento do array.
-
+const esparso = [10, 50, 110, 160, 200, 250, 330];
+console.log(esparso.length);
+// Adicionando elementos em determinado indice
+// 1º Argumento: Indice onde o novo elemento sera adicionado
+// 2º Argumento: O número de elementos a serem excluidos, nesse caso nenhum
+// 3º Argumento: O elemento a ser adicionado 
+esparso.splice(1,0,20);
+esparso.splice(3,0,125);
+esparso.splice(7,0,390);
+console.log(esparso);
+console.log(esparso.length);
 // Crie um programa em Node.js que inicie com um array cinco elementos e atribua um novo valor para a primeira posição (índice 0) desse array. Em seguida, exiba o array antes e depois da alteração.
 const elementos = [1, 2, 3, 4, 5];
 console.log(elementos);
@@ -18,4 +28,19 @@ console.log(meuArray);
 
 
 // Desenvolva um programa em Node.js que crie um array vazio e atribua valores a ele utilizando o método push(). Adicione três números inteiros ao array e, em seguida, exiba o array resultante.
+const arrayVazia = [];
+arrayVazia.push(100);
+arrayVazia.push(500);
+arrayVazia.push(1000);
+console.log(arrayVazia);
+
+
 // Desenvolva um programa em Node.js que simule uma clínica veterinária. Crie um array vazio chamado clinica que representará a fila de animais na clínica. Em um primeiro momento, simule a chegada de três animais diferentes e exiba a lista de animais no console. Após a exibição, remova os animais da lista um por vez e, por fim, exiba no console o estado final da lista.
+const clinica = ['Cachorro', 'Gato', 'Pato'];
+console.log(clinica);
+clinica.pop(); // Remove o ultimo elemento da lista
+console.log(clinica);
+clinica.pop();
+console.log(clinica);
+clinica.pop();
+console.log(clinica);
